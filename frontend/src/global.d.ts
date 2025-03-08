c43ce111aceb1d2ab04ee1fe26f474229ef3b83e
MLmarbles <22338705@studentmail.ul.ie>
2025-03-08 20:05:32 +0000

    Setup frontend structure with Vite Typescript

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,16 @@
+// src/global.d.ts
+/// <reference types="vite/client" />
+
+declare module "*.module.css" {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
+  
+  declare module "*.module.scss" {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
+  
+  declare module "*.css";
+  declare module "*.scss";
+  
