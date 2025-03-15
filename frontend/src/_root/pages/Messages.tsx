1d9e2decd9f0e7710235b8ac126bfadd675a4458
MLmarbles <22338705@studentmail.ul.ie>
2025-03-15 17:37:45 +0000

    fixing oopsie

--- frontend/src/_root/pages/Messages.tsx
+++ frontend/src/_root/pages/Messages.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Message = () => {
-  return <div>Message</div>;
+const Messages = () => {
+  return <div>Messages</div>;
 };
 
-export default Message;
+export default Messages;
