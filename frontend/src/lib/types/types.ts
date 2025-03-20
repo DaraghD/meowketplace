ae19f21aaef6eef6ec95c52f49a9ba0d87e5b4fb
MLmarbles <22338705@studentmail.ul.ie>
2025-03-20 17:04:49 +0000

    Added product and review types to frontend file

--- frontend/src/lib/types/types.ts
+++ frontend/src/lib/types/types.ts
@@ -12,3 +12,22 @@ export interface User {
     messageContent: string; 
     createdAt: Date; 
   }
+
+  export interface Product {
+    id: number;
+    user: User;
+    productText: string;
+    price: number;
+    starRating: number;
+    createdAt: Date;
+    reviews: Review[];
+}
+
+export interface Review {
+  id: number;
+  user: User;
+  product: Product;
+  reviewText: string;
+  starRating: number;
+  createdAt: Date;
+}
