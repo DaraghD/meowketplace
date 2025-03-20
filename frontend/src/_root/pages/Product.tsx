5f787850b4f0e3e2514ea52d853f94d3ac343de5
MLmarbles <22338705@studentmail.ul.ie>
2025-03-20 12:02:31 +0000

    Developing layout for page

--- frontend/src/_root/pages/Product.tsx
+++ frontend/src/_root/pages/Product.tsx
@@ -1,5 +1,33 @@
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      <div className="flex h-1/4">
+        <div className="flex flex-col w-1/2 p-5">
+          <img src="/assets/images/cat-brushing.jpg" />
+          <p>REVIEWS</p>
+        </div>
+        <div className="flex flex-col w-1/2 p-5">
+          <p>TITLE</p>
+          <p>DESCRIPTION</p>
+          <button className="hover:cursor-pointer">MESSAGE</button>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-col p-5">
+        <p>LATEST REVIEWS</p>
+        <p>
+          ALEX: UTTER DOGSHIT SERVICE{" "}
+          <span>
+            <button className="hover:cursor-pointer">MESSAGE</button>
+          </span>
+        </p>
+        <div className="flex p-5 justify-between">
+          <p>LOAD MORE...</p>
+          <p>LEAVE A REVIEW</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
