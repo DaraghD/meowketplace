a24963d871f2f00e7d3b402123ba74cb9b5b7f67
MLmarbles <22338705@studentmail.ul.ie>
2025-03-21 10:34:27 +0000

    bugfix: removed horizontal scrolling

--- frontend/src/_root/pages/ProductListing.tsx
+++ frontend/src/_root/pages/ProductListing.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 const ProductListing = () => {
-  return <div>ProductListing</div>;
+  return (
+    <div>
+      <div className="flex h-screen max-w-screen">
+        <div className="flex flex-col w-1/2 p-5">
+          <Button className="bg-white text-black">Add Image</Button>
+          <p>REVIEWS</p>
+        </div>
+        <div className="flex flex-col w-1/2 p-5">
+          <p>TITLE</p>
+          <p>DESCRIPTION</p>
+          <button className="hover:cursor-pointer">MESSAGE</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductListing;
