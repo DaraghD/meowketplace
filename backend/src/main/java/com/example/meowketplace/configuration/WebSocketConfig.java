e9e9816c0ff822d2a8f95852d340c783bc6d67e1
Daragh <esy@skynet.ie>
2025-03-30 23:15:18 +0100

    backend: message notification ws

---
+++ backend/src/main/java/com/example/meowketplace/configuration/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.meowketplace.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // Enable a simple message broker for topics
+        config.setApplicationDestinationPrefixes("/app"); // Set the prefix for messages handled by application
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // Register the endpoint for WebSocket connections
+    }
+}
