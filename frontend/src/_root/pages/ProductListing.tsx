4055eb36f8c7b26fcdcbe383e45553ea3ee18e37
MLmarbles <22338705@studentmail.ul.ie>
2025-03-31 21:19:20 +0100

    Resize file image input

--- frontend/src/_root/pages/ProductListing.tsx
+++ frontend/src/_root/pages/ProductListing.tsx
@@ -124,7 +124,7 @@ const ProductListing = () => {
     return (
         <div className="flex max-w-screen p-5">
             <div
-                className="flex flex-col h-4/5 w-1/2 justify-center items-center border border-dashed border-gray-400 cursor-pointer"
+                className="flex flex-col min-h-[400px] w-1/2 justify-center items-center border border-dashed border-gray-400 cursor-pointer"
                 onClick={handleDivClick} // Make the div clickable
             >
                 {selectedImages.length > 0 ? ( // Conditionally render the images or the button
