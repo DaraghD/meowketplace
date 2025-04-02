2dc36b55cf58428f60321431d8f466a79dfa811b
Daragh <esy@skynet.ie>
2025-04-02 11:14:34 +0100

    reviews: use reply userid for pfp

--- frontend/src/components/Reviews.tsx
+++ frontend/src/components/Reviews.tsx
@@ -372,7 +372,7 @@ const Reviews: React.FC<ReviewProps> = ({
                                             className="flex items-start gap-2"
                                         >
                                             <Avatar className="h-6 w-6">
-                                                <AvatarImage src={`http://localhost:8080/api/user/picture/${review.user_id}`} />{" "}
+                                                <AvatarImage src={`http://localhost:8080/api/user/picture/${reply.user_id}`} />{" "}
                                                 <AvatarFallback>
                                                     0
                                                 </AvatarFallback>
